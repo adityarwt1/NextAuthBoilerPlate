12561671578ea25d19b8b344bd71bc5c0507b05e
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-11 08:09:05 +0530

    initial of the signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div className='w-full text-center'>SignUp</div>
+        </>
+    )
+}
+
+export default page;
